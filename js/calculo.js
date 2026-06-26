da6bdaad498e0e07430b78052e5ea57d22ac00ce
devguilhrm <guilimadev19@gmail.com>
2026-06-26 14:33:45 -0300

    feat: -criada a função no script calculo que realiza o cálculo da área do imóvel para poder retornar nos resultados -criada a função de cálculo do valor do lote utilizando o parâmetro área para calcular,através da regra de negócio do cálculo da área ser multiplicado pelo valor do metro quadrado

---
+++ js/calculo.js
@@ -0,0 +1,9 @@
+const VALOR_POR_METRO = 550;
+
+export const calcularArea = (comprimento, largura) => {
+    return Number((comprimento * largura).toFixed(2));
+};
+
+export const calcularValorDoLote = (area) => {
+    return Number((area * VALOR_POR_METRO).toFixed(2));
+};
