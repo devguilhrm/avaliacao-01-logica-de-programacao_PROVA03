c3964361f2943f19e6b0c6fc67c050f78fc2672e
devguilhrm <guilimadev19@gmail.com>
2026-06-26 14:34:19 -0300

    feat: -criada a função para calcular o percentual adicional utilizando estrutura condicional organizada e com return para cobrir casos alternativos. - criada a função calcular valor adicional utilizando como parâmetro o valor do lote para utilizar a função anterior e poder retornar o valor final adicional a ser pago. - criada a função de formatação da moeda utilizando tolocalestring para converter strings em objetos formatados utilizando parâmetro de localidade 'pt-BR'

--- js/calculo.js
+++ js/calculo.js
@@ -7,3 +7,31 @@ export const calcularArea = (comprimento, largura) => {
 export const calcularValorDoLote = (area) => {
     return Number((area * VALOR_POR_METRO).toFixed(2));
 };
+
+export const calcularPercentualAdicional = (valorLote) => {
+    if (valorLote <= 20000) {
+        return 0;
+    }
+    if (valorLote <= 100000) {
+        return 0.05;
+    }
+    if (valorLote <= 500000) {
+        return 0.10;
+    }
+    if (valorLote <= 1000000) {
+        return 0.15;
+    }
+    return 0.20;
+};
+
+export const calcularValorAdicional = (valorLote) => {
+    const percentual = calcularPercentualAdicional(valorLote);
+    return Number((valorLote * percentual).toFixed(2));
+};
+
+export const formatarMoeda = (valor) => {
+    return valor.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    });
+};
